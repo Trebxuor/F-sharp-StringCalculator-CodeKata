1519f8dfd336fb6d9e3585f580f495e6c54aa110
Trebxuor <admin@trebxuor.co.za>
2017-04-28 13:21:42 +0200

    Initial Commit

---
+++ Kata2/Kata2/Program.fs
@@ -0,0 +1,31 @@
+﻿namespace Kata2
+open System.Text.RegularExpressions
+open System
+
+module Kata =
+    let convertArrayToRegex stringArray =
+        String.concat "|" (Array.filter (fun a -> a <> "") stringArray)
+
+    let convertToIntArrayAndSum stringArray =
+        let intNumberArray = Array.map int stringArray
+        let numbersSmallerThan1001 = Array.filter (fun a -> a < 1001) intNumberArray
+        let negatives = Array.filter (fun a -> a < 0) intNumberArray
+        match negatives.Length > 0 with
+        | true -> failwith (String.Join(";", negatives))
+        | false -> Array.sum numbersSmallerThan1001
+
+    let Add numbers =
+        match Seq.toList numbers with
+        | [] -> 0
+        | '/'::'/'::'['::remainder ->
+            let delimsAndNumbers = String.Concat remainder
+            let delimsString = delimsAndNumbers.Substring(0, delimsAndNumbers.IndexOf("\n"))
+            let numbersString = delimsAndNumbers.Substring(delimsAndNumbers.IndexOf("\n"))
+            let delimsArray = Regex.Split(delimsString, "\]\[|\]|\[")
+            convertToIntArrayAndSum (Regex.Split(numbersString, convertArrayToRegex delimsArray))
+        | '/'::'/'::delim::'\n'::remainder ->
+            let numberArray = Regex.Split(String.Concat remainder, string delim)
+            convertToIntArrayAndSum numberArray
+        | _ ->
+            let numberArray = Regex.Split(numbers, ",|\n")
+            convertToIntArrayAndSum numberArray
---
+++ Kata2/Kata2/Tests.fs
@@ -0,0 +1,48 @@
+﻿namespace Kata2
+
+module Tests =
+    open NUnit.Framework
+
+    [<Test>]
+    let ``For an empty string should return 0``() =
+        Assert.AreEqual(0, Kata.Add "")
+
+    [<Test>]
+    let ``For one number return thet number``() =
+        Assert.AreEqual(1, Kata.Add "1")
+
+    [<Test>]
+    let ``For mulitple numbers seperated by a comma return sum``() =
+        Assert.AreEqual(3, Kata.Add "1,2")
+
+    [<Test>]
+    let ``For multiple numbers seperated bya newline and comma return sum``() =
+        Assert.AreEqual(6, Kata.Add "1,2\n3")
+
+    [<Test>]
+    let ``For custom delimeters should produce a sum``() =
+        Assert.AreEqual(6, Kata.Add "//;\n1;2;3")
+
+    [<Test>]
+    let ``For any negative number throw an error with all in the message``() =
+        try
+            Kata.Add "-1,2,-3" |> ignore
+            Assert.Fail("No exception")
+        with
+        | Failure msg -> Assert.AreEqual("-1;-3", msg)
+
+    [<Test>]
+    let ``Ignore numbers larger than 1000 in sum``() =
+        Assert.AreEqual(4, Kata.Add "//;\n1;1001;3")
+
+    [<Test>]
+    let ``Custom delimeters of any length``() =
+        Assert.AreEqual(6, Kata.Add "//[abc]\n1abc2abc3")
+
+    [<Test>]
+    let ``Multiple delimeters of single lenght``() =
+        Assert.AreEqual(6, Kata.Add "//[a][b]\n1a2b3")
+
+    [<Test>]
+    let ``Multiple delimeters of multiple length``() =
+        Assert.AreEqual(6, Kata.Add "//[abc][cba]\n1abc2cba3")
